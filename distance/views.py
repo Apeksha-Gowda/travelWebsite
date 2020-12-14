0cddda04f5108e8cc86cc8f6a9f8bbc1edb0f55e
mahidharseelam <44781598+mahidharseelam@users.noreply.github.com>
2020-12-14 12:01:10 -0500

    coupon and location validation added
    small error message appears beneath the input text box

--- distance/views.py
+++ distance/views.py
@@ -13,6 +13,7 @@ def calculate_distance_view(request):
     #initial values
     distance_output = None
     destination = None
+    error = None
     obj = get_object_or_404(Distance, id=1)
     form = MeasurementModelForm(request.POST or None)
     geoLocator = Nominatim(user_agent='distance')
@@ -44,38 +45,45 @@ def calculate_distance_view(request):
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data.get('destination')
-        destination = geoLocator.geocode(destination_)
-        #print(destination)
-        
-        # destination co-ordinates
-        d_lat = destination.latitude
-        d_lon = destination.longitude
 
-        pointB = (d_lat,d_lon )
+        try:
+            destination = geoLocator.geocode(destination_)
+        
+            #print(destination)
+             
+            # destination co-ordinates
+            d_lat = destination.latitude
+            d_lon = destination.longitude
 
-        #distance calculation
-        distance_output = round(geodesic(pointA, pointB).km,2)
+            pointB = (d_lat,d_lon )
+
+            #distance calculation
+            distance_output = round(geodesic(pointA, pointB).km,2)
+            
+            #map modification
+            m = folium.Map(width = 800, height = 500, location=get_center_coordinates(l_lat,l_lon,d_lat,d_lon), zoom_start=get_zoom(distance_output))
         
-        #map modification
-        m = folium.Map(width = 800, height = 500, location=get_center_coordinates(l_lat,l_lon,d_lat,d_lon), zoom_start=get_zoom(distance_output))
-    
-        #location marker
-        folium.Marker([l_lat,l_lon], tooltip = 'click here for more', popup =city['city'],
-                    icon= folium.Icon(color='purple')).add_to(m)
-        
-        #draw the line between the location and destination
-        line = folium.PolyLine(locations=[pointA, pointB],weight=5, color='blue')
-        m.add_child(line)
-        
-        #destination marker
-        folium.Marker([d_lat,d_lon], tooltip = 'click here for more', popup =destination,
-                    icon= folium.Icon(color='red',icon='cloud')).add_to(m)
-        instance.location = location
-        instance.distance = distance_output
-        instance.save()
+            #location marker
+            folium.Marker([l_lat,l_lon], tooltip = 'click here for more', popup =city['city'],
+                        icon= folium.Icon(color='purple')).add_to(m)
+            
+            #draw the line between the location and destination
+            line = folium.PolyLine(locations=[pointA, pointB],weight=5, color='blue')
+            m.add_child(line)
+            
+            #destination marker
+            folium.Marker([d_lat,d_lon], tooltip = 'click here for more', popup =destination,
+                        icon= folium.Icon(color='red',icon='cloud')).add_to(m)
+            instance.location = location
+            instance.distance = distance_output
+            instance.save()
+        except:
+            error = "Location not found"
+            print(error)
 
     m = m._repr_html_()
     context = {
+        'error':error,
         'distance' : distance_output,
         'form' : form,
         'map' : m,
